368006aa55275ff853b1bf892c91918c74db9325
Stig Nervik <stig.nervik@gmail.com>
2017-12-21 14:45:09 +0100

    Add switching between status views.

---
+++ playground-react/src/components/AlarmView.js
@@ -0,0 +1,16 @@
+import react, {Compoinent} from 'react'
+
+class AlarmView extends Component {
+
+
+    render() {
+        return (
+            <div className="AlarmView">
+                Alarm View
+            </div>
+        );
+    }
+
+}
+
+export default AlarmView;
--- playground-react/src/components/StatusView.js
+++ playground-react/src/components/StatusView.js
@@ -3,10 +3,21 @@ import React, {Component} from 'react'
 class StatusView extends Component {
 
     render() {
+        let view = null;
+        if (this.props.statusViewSelected === "ALARM") {
+            view = "Alarm";
+        }
+        else if (this.props.statusViewSelected === "FAULT") {
+            view = "Fault";
+        }
+        else if (this.props.statusViewSelected === "DISABLE") {
+            view = "Disable";
+        }
+
         return (
             <div className="statusView">
 
-                {this.props.statusViewSelected ? "StatusView" : null}                    
+                {view}
             </div>
         );
     }
